64e9a74c1269e9574ed5016da08fcb8772ea31ae
Andrii Petrenko <aplsms@gmail.com>
2024-08-31 10:34:08 -0700

    working relrase

--- m4b.py
+++ m4b.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # This file is in the Public Domain.
@@ -17,42 +17,55 @@
 #       iTunes for transferring the audio book.
 #
 # Requires: ffmpeg, MP4Box, mp4chaps, mutagen, libmad, mp3wrap
+#  pip3 install --global-option=build_ext --global-option="-I/opt/homebrew/include/" --global-option="-L/opt/homebrew/lib" pymad
+
 
 from mutagen.easyid3 import EasyID3
 
 from mutagen.mp3 import MP3
 from mutagen.mp4 import MP4
 
-import mad
+import sox
 import time
 import os
 import shutil
 import subprocess
 import sys
-import getopt
-import datetime
 import tempfile
+import logging
+import unicodedata
+import argparse
+import re
 
 # brew install ffmpeg mp3wrap mad gpac
 
-OUTPUT_DIR = "./output"
+OUTPUT_DIR = "./"
+OUTPUT_TREE = False
 COVER      = ''
 mp3_files  = ''
 AUTHOR     = ''
 TITLE      = ''
 ALBUM      = ''
+SERIES     = ''
+NARRATORS  = ''
 DEBUG      = ''
 TEMP       = tempfile.mkdtemp();
 TFILE      = ''
 BITRATE    = '128k'
-Version    = '0.1.0'
+Version    = '0.5.0'
+COMMENT    = ''
+SPEAKER    = ''
+SERIES     = ''
+LOGLEVEL   = logging.WARNING
+MOVE     = False
+RENAME   = False
+DRYRUN   = False
+INPUT_DIR = './'
+FNULL = open(os.devnull, 'w')
+filename   = ''
+mp3stmp = []
+DIR=''
 
-argv = sys.argv[1:]
-
-def debug(msg):
-    if DEBUG :
-        today = datetime.datetime.today()
-        print today.strftime('%Y-%m-%dT%H:%M:%S.%f')+" [" + sys.argv[0] + "] " + msg
 
 def secs_to_hms(seconds):
     h, m, s, ms = 0, 0, 0, 0
@@ -69,67 +82,271 @@ def secs_to_hms(seconds):
         pass
     return "%.2i:%.2i:%.2i.%s" % (h, m, s, ms)
 
-print "mp3 to m4b converter by Andrii Petrenko (apl@petrenko.me)"
+def mkpath(tags):
+    logging.debug(f"Creating directory name by tags")
 
-try:
-    opts, args = getopt.getopt(argv,"hi:c:a:t:A:df",["ifile=","cover=","author=","title="])
-except getopt.GetoptError:
-    print 'm4b.py [-i <inputfile>[,<inputfile2>...]] [-c <cover>] [-a <author>] [-A <Album>] [-t <title>] -d -h -f'
-    sys.exit(2)
-for opt, arg in opts:
-    if opt == '-h':
-        print 'm4b.py [-i <inputfile>[,<inputfile2>...]] [-c <cover>] [-a <author>] [-A <Album>] [-t <title>] -d -h -f'
-        sys.exit()
-    elif opt == '-d':
-        DEBUG=1
-    elif opt in ("-i", "--ifile"):
-        mp3_files = arg.split(',')
-    elif opt in ("-c", "--cover"):
-        COVER = arg
-        if not os.path.isfile(COVER):
-            print "Cover (" + COVER + ") does not exist"
-            sys.exit(2)
-        debug("Cover file is " + COVER )
-    elif opt in ("-a", "--author"):
-        AUTHOR = arg.decode('utf-8')
-    elif opt in ("-A", "--album"):
-        ALBUM = arg.decode('utf-8')
-    elif opt in ("-t", "--title"):
-        TITLE = arg.decode('utf-8')
-    elif opt in ("-f", "--file"):
-        TFILE = 'true'
+    if '\xa9ART' not in tags:
+        print("Please set Author")
+        os.exit(99)
 
-debug("Temporary directory is " + TEMP)
+    author = tags['\xa9ART'][0]
 
-# # output dir
-# if not os.path.isfile("output"):
-#     os.mkdir("output")
+    if '\xa9nam' not in tags:
+        print("Please set Name")
+        os.exit(99)
+    name = tags['\xa9nam'][0]
 
-# get mp3
-if not mp3_files :
-    mp3_files = [filename for filename in os.listdir(".") if filename.endswith(".mp3") or filename.endswith(".MP3")]
+    series = ''
+    if "tvsh" in tags and tags['tvsh']:
+        series = tags['tvsh'][0]
+
+    album = ''
+    if "\xa9alb" in tags and tags['\xa9alb']:
+        album = tags['\xa9alb'][0]
+    else:
+        album = name
+
+    speaker = ''
+    if '\xa9wrt' in tags and tags['\xa9wrt']:
+        speaker=tags['\xa9wrt'][0]
+
+    comment = ''
+    if '\xa9cmt' in tags and tags['\xa9cmt']:
+        comment=tags['\xa9cmt'][0]
+
+    logging.debug(f"Title:   {name}")
+    logging.debug(f"album:   {album}")
+    logging.debug(f"Series:  {series}")
+    logging.debug(f"Speaker: {speaker}")
+    logging.debug(f"Comment: {comment}")
+
+    if album and series and album == name:
+        #cut series from name
+        k = re.compile( "%s.? (.+)" % (series) )
+        pname=name
+        if k.match(name):
+            pname = k.search( str(name) ).group(1)
+        logging.debug("Dir [album and series, album == title]: %s/%s/%s" % (author, series, pname ))
+        DIR= "%s/%s/%s" % (author, series, pname )
+
+    elif album and series and album != name:
+        #cut series from name
+        # k = re.compile( "%s.? (.+)" % (series) )
+        # pname=name
+        # if k.match(name):
+        #     pname = k.search( str(name) ).group(1)
+        logging.debug("Dir [album and series, album != title]: %s/%s/%s" % (author, series, album ))
+        DIR= "%s/%s/%s" % (author, series, album )
+
+    elif album and not series:
+        logging.debug("Dir [album and not series]: %s/%s" % (author, album ))
+        DIR= "%s/%s" % (author, album )
+
+    elif series and not ALBUM:
+        # Cut series name from File name
+        k = re.compile( "%s.? (.+)" % (series) )
+        pname=name
+        if k.match(name):
+            pname = k.search( str(name) ).group(1)
+        DIR= "%s/%s/%s" % (author, series, pname )
+    else:
+        DIR= "%s/%s" % (author, pname )
+    logging.debug(f"Path   : {DIR}")
+    return DIR
+
+def mkfilename(tags):
+    logging.debug(f"Creating file name by tags")
+
+    if '\xa9ART' not in tags:
+        print("Please set Author")
+        os.exit(99)
+
+    author = tags['\xa9ART'][0]
+
+    if '\xa9nam' not in tags:
+        print("Please set Name")
+        os.exit(99)
+    name = tags['\xa9nam'][0]
+
+    series = ''
+    if "tvsh" in tags and tags['tvsh']:
+        series = tags['tvsh'][0]
+
+    album = ''
+    if "\xa9alb" in tags and tags['\xa9alb']:
+        album = tags['\xa9alb'][0]
+    else:
+        album = name
+
+    speaker = ''
+    if '\xa9wrt' in tags and tags['\xa9wrt']:
+        speaker=tags['\xa9wrt'][0]
+
+    comment = ''
+    if '\xa9cmt' in tags and tags['\xa9cmt']:
+        comment=tags['\xa9cmt'][0]
+
+    logging.debug(f"Title:   {name}")
+    logging.debug(f"album:   {album}")
+    logging.debug(f"Series:  {series}")
+    logging.debug(f"Speaker: {speaker}")
+    logging.debug(f"Comment: {comment}")
+
+    # Making new file name
+
+    if name != album:
+        k = re.compile( "%s. (.+)" % (album) )
+        pname=name
+        if k.match(name):
+            pname = k.search( str(name) ).group(1)
+        name = "%s - %s" % (album, pname)
+    if series:
+        k = re.compile( "%s.? (.+)" % (series) )
+        pname=name
+        if k.match(name):
+            pname = k.search( str(name) ).group(1)
+        filename =  "%s - %s. %s" % (author, series, pname)
+    else:
+        filename =  "%s - %s" % (author, name)
+    if speaker:
+        filename = filename + " [%s]" % (speaker)
+    if comment:
+        filename = filename + " (%s)" % (comment)
+    filename=filename+".m4b"
+    logging.debug(f"Filename: {filename}")
+
+    return filename
+
+
+print("mp3 to m4b converter by Andrii Petrenko (apl@petrenko.me)")
+
+parser = argparse.ArgumentParser(
+        prog='2m4b',
+        description='mp3 to m4b converter by Andrii Petrenko (apl@petrenko.me)',
+        epilog='-=*=-')
+
+
+parser.add_argument('-A', '--album',   dest="album",   help="Audiobook album name")
+parser.add_argument('-a', '--author',  dest="author",  help='Audiobook Author' )
+parser.add_argument('-C', '--comment', dest="comment", help="Audiobook Comment")
+parser.add_argument('-c', '--cover',   dest="cover",   help="Audiobook cover ")
+parser.add_argument('-o', '--output',  dest="output",  help="destination directory for file output")
+parser.add_argument('-S', '--series',  dest="series",  help="Audiobook Series")
+parser.add_argument('-s', '--speaker', dest="speaker", help="Audiobook Speaker/Narrover")
+parser.add_argument('-t', '--title',   dest="title",   help="Audiobook Title")
+parser.add_argument('-m', '--move',    dest='move',    action='store_true', help='Move file to the proper directoy structure')
+parser.add_argument('-d',              dest='dryrun',  action='store_true', help='DryRun action. Do not change anythiing')
+parser.add_argument('-v', '--verbose', dest="verbose", action='store_true', help="verbose output")
+parser.add_argument(      '--debug',   dest='debug' ,  action='store_true', help='Debug output')
+parser.add_argument('filenames', type=str, nargs='+', help='Audiobook filenames',)
+
+args = parser.parse_args()
+
+
+DRYRUN     = args.dryrun
+VERBOSE    = logging.INFO  if args.verbose        else LOGLEVEL
+LOGLEVEL   = logging.DEBUG if args.debug          else LOGLEVEL
+COVER      = args.cover    if args.cover          else COVER
+AUTHOR     = args.author   if args.author         else AUTHOR
+ALBUM      = args.album    if args.album          else ALBUM
+TITLE      = args.title    if args.title          else TITLE
+COMMENT    = args.comment  if args.comment        else COMMENT
+COMMENT    = ''            if args.comment == '-' else COMMENT
+SERIES     = args.series   if args.series         else SERIES
+SPEAKER    = args.speaker  if args.speaker        else SPEAKER
+SPEAKER    = ''            if args.speaker == '-' else SPEAKER
+OUTPUT_DIR = args.output   if args.output         else OUTPUT_DIR
+MOVE       = args.move
+FILENAMES  = args.filenames
+
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=LOGLEVEL)
+
+if os.path.isdir(FILENAMES[0]) and len(FILENAMES) == 1:
+    #We got single  directory
+    INPUT_DIR  = args.filenames[0]
+    print(INPUT_DIR)
+    FILENAMES = []
+    mp3_files = [filename for filename in os.listdir(INPUT_DIR+"/") if filename.endswith(".mp3") or filename.endswith(".MP3")]
     mp3_files.sort()
+    for i in mp3_files:
+        FILENAMES.append(INPUT_DIR+"/"+i)
+    msg=f"Wrapping files from {INPUT_DIR}: "+ str(", " .join(FILENAMES))
+    logging.debug(msg)
+    logging.debug ("mp3wrap -v " +TEMP+ "/output.mp3 " + " ".join(FILENAMES) )
+    subprocess.call(["mp3wrap"] + ["-v"] + [TEMP+"/output.mp3"] + FILENAMES, stdout=FNULL)
 
-FNULL = open(os.devnull, 'w')
+elif os.path.isfile(FILENAMES[0]) and len(FILENAMES) == 1:
+    #single file, lets speedup
+    logging.debug( f"Single file -- just copy {args.filenames[0]} to {TEMP}/output_MP3WRAP.mp3")
+    shutil.copy (args.filenames[0], f"{TEMP}/output_MP3WRAP.mp3")
+
+elif len(FILENAMES) > 255:
+    # too many mp3 files
+    logging.error("mp3wrap accept only less than 255 files")
+    sys.exit(2)
+
+else:
+    #regular files
+    msg=f"Wrapping files from {INPUT_DIR}: "+ str(", " .join(FILENAMES))
+    logging.debug(msg)
+    logging.debug ("mp3wrap -v " +TEMP+ "/output.mp3 " + " ".join(FILENAMES) )
+    subprocess.call(["mp3wrap"] + ["-v"] + [TEMP+"/output.mp3"] + FILENAMES, stdout=FNULL)
+
+logging.warning("Wrapping mp3 files complete")
+
+
+# checking cover if exist
+COVER = args.cover
+if args.cover and not os.path.isfile(COVER):
+    logging.error("Cover (" + COVER + ") does not exist")
+    sys.exit(2)
+elif not args.cover:
+    COVER = INPUT_DIR+"/cover.jpg"  if os.path.isfile(INPUT_DIR+"/cover.jpg" ) else ''
+    COVER = INPUT_DIR+"/Cover.jpg"  if os.path.isfile(INPUT_DIR+"/Cover.jpg" ) else ''
+    COVER = INPUT_DIR+"/folder.jpg" if os.path.isfile(INPUT_DIR+"/folder.jpg") else ''
+    COVER = INPUT_DIR+"/Folder.jpg" if os.path.isfile(INPUT_DIR+"/Folder.jpg") else ''
+    COVER = INPUT_DIR+"/front.jpg"  if os.path.isfile(INPUT_DIR+"/front.jpg" ) else ''
+    COVER = INPUT_DIR+"/Front.jpg"  if os.path.isfile(INPUT_DIR+"/Front.jpg" ) else ''
+    COVER = INPUT_DIR+"/cover.png"  if os.path.isfile(INPUT_DIR+"/cover.png" ) else ''
+    COVER = INPUT_DIR+"/Cover.png"  if os.path.isfile(INPUT_DIR+"/Cover.png" ) else ''
+    COVER = INPUT_DIR+"/folder.png" if os.path.isfile(INPUT_DIR+"/folder.png") else ''
+    COVER = INPUT_DIR+"/Folder.png" if os.path.isfile(INPUT_DIR+"/Folder.png") else ''
+    COVER = INPUT_DIR+"/front.png"  if os.path.isfile(INPUT_DIR+"/front.png" ) else ''
+    COVER = INPUT_DIR+"/Front.png"  if os.path.isfile(INPUT_DIR+"/Front.png" ) else ''
+
+    if not COVER:
+        logging.debug("trying to get Cover from 1st mp3 file")
+        if LOGLEVEL == logging.DEBUG:
+            # temporary subprocess.call(["ffmpeg"] + ["-i"] + [FILENAMES[0]] + [TEMP+"/cover.png"])
+            subprocess.call(["ffmpeg"] + ["-i"] + [FILENAMES[0]] + [TEMP+"/cover.png"])
+        else:
+            subprocess.call(["ffmpeg"] + ["-i"] + [FILENAMES[0]] + [TEMP+"/cover.png"], stdout=FNULL, stderr=FNULL)
+        if os.path.isfile(TEMP+"/cover.png"):
+            COVER = TEMP+"/cover.png"
+
+logging.debug("Cover file is: " + COVER )
+logging.debug("Temporary directory is " + TEMP)
+
 
 #debug ("Input *.mp3 file list is " + ",".join(mp3_files));
-print "Converting " + ",".join(mp3_files)+" to audiobook"
+logging.warning("Converting " + ",".join(FILENAMES)+" to audiobook")
 
 # wrap mp3
-debug ("mp3wrap -v " +TEMP+ "/output.mp3 " + ",".join(mp3_files) )
-subprocess.call(["mp3wrap"] + ["-v"] + [TEMP+"/output.mp3"] + mp3_files, stdout=FNULL)
-print "Done"
+#todo: do not do that if dryrun ??
+# if len(args.filenames) == 1:
+
 
 # create chapters file
 chapters_file = open(TEMP+'/chapters', 'w')
 counter = 0
 time    = 0
 
-
-for filename in mp3_files:
+for filename in FILENAMES:
     audio = MP3(filename, ID3=EasyID3)
-    length = mad.MadFile(filename).total_time() / 1000. # don't use mutagen here, because it isn't very precise
-    if audio.info.bitrate >= 160000:
+    length = sox.file_info.duration(filename) # don't use mutagen here, because it isn't very precise
+    logging.debug(f"File {filename} length is {length} sec")
+    logging.debug(audio.pprint())
+    if audio.info.bitrate   >= 160000:
         BITRATE = '160k'
     elif audio.info.bitrate >= 128000:
         BITRATE = '128k'
@@ -138,11 +355,21 @@ for filename in mp3_files:
     else:
         BITRATE = '64k'
 
+    logging.debug(f"File {filename} bitrate is {BITRATE}")
+
     if not TITLE:
         try:
             TITLE = audio["album"][0]
         except:
             pass
+
+    if not TITLE:
+        try:
+            TITLE = audio["title"][0]
+        except:
+            pass
+
+        logging.debug(f"File {filename} Title is {TITLE}")
 
     if not AUTHOR:
         try:
@@ -150,92 +377,121 @@ for filename in mp3_files:
         except:
             pass
 
+    logging.debug(f"File {filename} Author is {AUTHOR}")
+
     if TFILE:
-        title = filename.decode('utf-8')
+        title = os.path.splitext(os.path.basename(filename))[0]
     else:
         try:
             title = audio["title"][0]
+            if len(title) > 90:
+                title = title[:90]
         except:
-            title = filename.decode('utf-8')
+            title = os.path.splitext(os.path.basename(filename))[0]
+
+    logging.debug(f"File {filename} chapter name is {title}")
 
     counter += 1
-
     chapters_file.write("CHAPTER%i=%s\n" % (counter, secs_to_hms(time)))
-    chapters_file.write("CHAPTER%iNAME=%s\n" % (counter, title.encode('utf-8')))
-    debug( "Chapter: " +str(counter)+", length: " +str(length)+ ", Title: " +title.encode('utf-8') )
-
+    chapters_file.write("CHAPTER%iNAME=%s\n" % (counter, title))
+    logging.debug( "Chapter: " +str(counter)+", length: " +str(length)+ ", Title: " + title )
     time += length
-debug( "BITRATE = "+BITRATE)
+
 chapters_file.close()
 
+if not AUTHOR:
+    logging.error("Please set Author")
+    sys.exit(99)
+
+
 # convert to aac
-ffmpeg = 'ffmpeg -i '+TEMP+'/output_MP3WRAP.mp3 -y -vn -acodec libfdk_aac -ab '+BITRATE+' -ar 44100 -f mp4 -threads 4 '+TEMP+'/output.aac'
-debug(ffmpeg)
-subprocess.call(ffmpeg.split(" "), stdout=FNULL, stderr=FNULL)
+ffmpeg = 'ffmpeg -i '+TEMP+'/output_MP3WRAP.mp3 -y -vn -acodec libfdk_aac -ab '+BITRATE+' -ar 44100 -f mp4 -threads 8 '+TEMP+'/output.aac'
+logging.debug(ffmpeg)
+if LOGLEVEL == logging.DEBUG:
+    subprocess.call(ffmpeg.split(" "))
+else:
+    subprocess.call(ffmpeg.split(" "), stdout=FNULL, stderr=FNULL)
 
 if not ALBUM:
     ALBUM = TITLE
 
 # add chapters
-debug("Adding Chapters")
-if DEBUG:
+logging.debug("Adding Chapters")
+if LOGLEVEL == logging.DEBUG:
     subprocess.call(["MP4Box", "-add", TEMP+"/output.aac", "-chap", TEMP+"/chapters", TEMP+"/output.mp4"])
 else:
     subprocess.call(["MP4Box", "-add", TEMP+"/output.aac", "-chap", TEMP+"/chapters", TEMP+"/output.mp4"], stdout=FNULL, stderr=FNULL)
 
 # convert chapters to quicktime format
-debug ("converting to the Quicktime Format")
-if DEBUG:
+logging.debug ("converting to the Quicktime Format")
+if LOGLEVEL == logging.DEBUG:
     subprocess.call(["mp4chaps", "--convert", "--chapter-qt", TEMP+"/output.mp4"])
 else:
     subprocess.call(["mp4chaps", "--convert", "--chapter-qt", TEMP+"/output.mp4"], stdout=FNULL)
 
 # create tags, rename file
-debug("Adding tags")
+logging.debug("Adding tags")
+
+logging.debug(f"TITLE   : {TITLE}")
+logging.debug(f"ALBUM   : {ALBUM}")
+logging.debug(f"AUTHOR  : {AUTHOR}")
+logging.debug(f"COMMENT : {COMMENT}")
+logging.debug(f"SPEAKER : {SPEAKER}")
+logging.debug(f"SERIES  :  {SERIES}")
+
+# create tags, rename file
 audio = MP4(TEMP+"/output.mp4")
 audio["\xa9nam"] = [TITLE]
 audio["\xa9alb"] = [ALBUM]
 audio["\xa9ART"] = [AUTHOR]
+audio["\xa9cmt"] = [COMMENT]
+audio['\xa9wrt'] = [SPEAKER]
+audio['aART']    = [SPEAKER]
+audio['tvsh']    = [SERIES]
+
+tags=audio.tags
+
 audio.save()
 
-if not COVER:
-    if os.path.isfile("cover.jpg"):
-        COVER = "cover.jpg"
-    elif os.path.isfile("Cover.jpg"):
-        COVER = "Cover.jpg"
-    elif os.path.isfile("folder.jpg"):
-        COVER = "folder.jpg"
-    elif os.path.isfile("front.jpg"):
-        COVER = "front.jpg"
-    else:
-        if DEBUG:
-            subprocess.call(["ffmpeg"] + ["-i"] + [mp3_files[0]] + [TEMP+"/cover.png"])
-        else:
-            subprocess.call(["ffmpeg"] + ["-i"] + [mp3_files[0]] + [TEMP+"/cover.png"], stdout=FNULL, stderr=FNULL)
-            if os.path.isfile(TEMP+"/cover.png"):
-                COVER = TEMP+"/cover.png"
-
 if COVER:
-    if DEBUG:
-        debug("Cover is: "+COVER)
+    if LOGLEVEL == logging.DEBUG:
+        logging.debug("Adding Cover is: "+COVER)
         subprocess.call(["mp4art", "--add", COVER, TEMP+"/output.mp4"])
     else:
         subprocess.call(["mp4art", "--add", COVER, TEMP+"/output.mp4"], stdout=FNULL)
 
+newfile=mkfilename(tags)
+newpath=mkpath(tags)
 
-if TITLE == ALBUM:
-    debug("Rename "+TEMP+"/output.mp4 " +AUTHOR+" - "+TITLE+".m4b" )
-    shutil.move(TEMP+"/output.mp4", "%s - %s.m4b" % (AUTHOR, TITLE))
-    print "Please check result: %s - %s.m4b" % (AUTHOR, TITLE)
+
+if MOVE:
+    DIR=f"{OUTPUT_DIR}/{newpath}"
 else:
-    debug("Rename "+TEMP+"/output.mp4 " +AUTHOR+" - "+ALBUM+" - "+TITLE+".m4b" )
-    shutil.move(TEMP+"/output.mp4", "%s - %s - %s.m4b" % (AUTHOR, ALBUM, TITLE))
-    print "Please check result: %s - %s - %s.m4b" % (AUTHOR, ALBUM, TITLE)
+    DIR=f"{OUTPUT_DIR}"
+
+logging.debug(f"Saving Audiobook to {DIR}/{newfile}")
+logging.debug(f"Creating directory {DIR}")
+if not os.path.isdir(f"{DIR}"):
+    try:
+        logging.debug("Creating directory {DIR}")
+        os.makedirs(f"{DIR}")
+    except OSError as error:
+        print(error)
+        sys.exit("99")
+
+logging.debug(f"Move {TEMP}/output.mp4 to {DIR}/{newfile}" )
+
+shutil.move(f"{TEMP}/output.mp4", f"{DIR}/{newfile}")
+
+print(f"Please check result:{DIR}/{newfile}")
+
+if INPUT_DIR != "./":
+    shutil.move (INPUT_DIR, INPUT_DIR+".Done")
 
 # cleanup
 
-if DEBUG:
-    debug("see temp directory: "+TEMP)
+if LOGLEVEL == logging.DEBUG:
+    logging.debug("see temp directory: "+TEMP)
     sys.exit()
 else:
     os.remove(TEMP+"/chapters")
